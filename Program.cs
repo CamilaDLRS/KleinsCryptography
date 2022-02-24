0287a628b379fe7f1049ff3bbdc74e2ab5190303
camila.delarosa@pneuzao.com.br <camila.delarosa@pneuzao.com.br>
2022-02-24 19:18:09 -0300

    correção do nome

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace CryptographyKlen
+namespace KleinsCryptography
 {
     /// <summary>
     /// Create by Camila Delarosa, based on Carlos Klein code
